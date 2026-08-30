6458babe15fb963a46b086951118b630a9764bf2
Ibrahim muhd <balaibrahimmuhammad250@gmail.com>
2026-08-30 17:44:50 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,32 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyAEWIrc0uCIHvJ_RHYW3l8UTumzYWeNo-w",
-
-  authDomain: "maja-a6299.firebaseapp.com",
-
-  projectId: "maja-a6299",
-
-  storageBucket: "maja-a6299.firebasestorage.app",
-
-  messagingSenderId: "217642905179",
-
-  appId: "1:217642905179:web:683d1c3e8c6fab3356997e"
-
+  apiKey: "AIzaSyAUQm-3ullzFDRwn5Iu0Hg_7u0Mfiz688o4",
+  authDomain: "maja-1fa6a.firebaseapp.com",
+  projectId: "maja-1fa6a",
+  messagingSenderId: "877281790124",
+  appId: "1:877281790124:web:14f693dfd580451e3bb709"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-
 const db = getFirestore(app);
 
-
-export { auth, db };
+export { app, auth, db };
